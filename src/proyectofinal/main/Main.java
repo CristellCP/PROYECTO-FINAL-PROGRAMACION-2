d94ccd5da8942059ebd2136e09cbd9f84884cd75
CristellCP <cristellcedeno@gmail.com>
2026-04-08 13:45:56 -0500

    Creando un objeto de la clase MenuPrincipal y llamando a su metodo

--- src/proyectofinal/main/Main.java
+++ src/proyectofinal/main/Main.java
@@ -2,6 +2,9 @@ package proyectofinal.main;
 
 public class Main {
     public static void main(String[] args) {
+        MenuPrincipal mp = new MenuPrincipal();
+        mp.MenuPrincipal();
+        
         System.out.println("Funcionando.");
     }
 }
