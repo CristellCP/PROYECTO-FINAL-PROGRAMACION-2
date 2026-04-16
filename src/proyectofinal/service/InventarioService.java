3d8881ce88014bbf7de9c8fe2d51445d92dc436e
ericarce059-prog <ericarce059@gmail.com>
2026-04-16 17:13:46 -0500

    Update InventarioService.java

---
+++ src/proyectofinal/service/InventarioService.java
@@ -0,0 +1,47 @@
+package proyectofinal.service;
+
+import java.util.*;
+
+public class InventarioService {
+
+    private MotorFile motorFile = new MotorFile();
+
+    public List<Motor> listarMotores() {
+        return motorFile.leerMotores();
+    }
+
+    public void agregarMotor(Motor nuevo) {
+        List<Motor> lista = motorFile.leerMotores();
+        lista.add(nuevo);
+        motorFile.guardarMotores(lista);
+    }
+
+    public Motor buscarMotor(String codigo) {
+        for (Motor m : motorFile.leerMotores()) {
+            if (m.getCodigoMotor().equalsIgnoreCase(codigo)) {
+                return m;
+            }
+        }
+        return null;
+    }
+
+    public void descontarStock(String codigo, int cantidad) {
+        List<Motor> lista = motorFile.leerMotores();
+
+        for (Motor m : lista) {
+            if (m.getCodigoMotor().equalsIgnoreCase(codigo)) {
+                m.setCantidadDisponible(m.getCantidadDisponible() - cantidad);
+            }
+        }
+
+        motorFile.guardarMotores(lista);
+    }
+
+    public void verificarStockBajo() {
+        for (Motor m : motorFile.leerMotores()) {
+            if (m.getCantidadDisponible() <= m.getStockMinimo()) {
+                System.out.println("⚠ Stock bajo: " + m.getCodigoMotor());
+            }
+        }
+    }
+}
