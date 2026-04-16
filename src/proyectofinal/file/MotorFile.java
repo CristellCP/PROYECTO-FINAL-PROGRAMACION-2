189cb1c2a52cbdf81b348e137b0bee9d8410a0b6
ericarce059-prog <ericarce059@gmail.com>
2026-04-16 16:53:13 -0500

    Update MotorFile.java

--- src/proyectofinal/file/MotorFile.java
+++ src/proyectofinal/file/MotorFile.java
@@ -1,5 +1,42 @@
 package proyectofinal.file;
 
 public class MotorFile {
-    
-}
+    private String ruta = "data/inventario.csv";
+
+
+    public List<Motor> leerMotores() {
+        List<Motor> lista = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(ruta))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] datos = linea.split(",");
+
+                Motor m = new Motor(
+                    datos[0],
+                    datos[1],
+                    datos[2],
+                    Double.parseDouble(datos[3]),
+                    Integer.parseInt(datos[4]),
+                    Integer.parseInt(datos[5])
+                );
+
+                lista.add(m);
+            }
+        } catch (IOException e) {
+            System.out.println("Error leyendo archivo");
+        }
+
+        return lista;
+    }
+
+    public void guardarMotores(List<Motor> lista) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(ruta))) {
+            for (Motor m : lista) {
+                bw.write(m.toString());
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error guardando archivo");
+        }
+    }}
