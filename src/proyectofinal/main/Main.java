c4541a76fa817745ab7b3137576b3cc3ea06b5cc
Luis-ulatina <luiz_c23@hotmail.com>
2026-04-17 18:41:06 -0500

    Activacion del sistema automatico

--- src/proyectofinal/main/Main.java
+++ src/proyectofinal/main/Main.java
@@ -1,12 +1,17 @@
 package proyectofinal.main;
 
+import proyectofinal.service.SistemaAlertas;
 import proyectofinal.ui.MenuPrincipal;
 
 public class Main {
     public static void main(String[] args) {
+        // ACTIVACIÓN DEL SISTEMA AUTOMÁTICO
+        SistemaAlertas sistema = new SistemaAlertas();
+        sistema.iniciarMonitoreoAutomatico(); 
 
         MenuPrincipal menu = new MenuPrincipal();
         menu.mostrarMenu();
-
+        
+        System.out.println("Funcionando.");
     }
 }
