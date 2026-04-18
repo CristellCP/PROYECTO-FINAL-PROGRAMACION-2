e4a0fe3df324b071d81adc9d624f292aea9467e2
Ana Freitez <freitezana1217@gmail.com>
2026-04-17 19:45:20 -0500

    Pruebas finales

--- src/proyectofinal/main/Main.java
+++ src/proyectofinal/main/Main.java
@@ -1,14 +1,9 @@
 package proyectofinal.main;
 
-import proyectofinal.service.SistemaAlertas;
 import proyectofinal.ui.MenuPrincipal;
 
 public class Main {
     public static void main(String[] args) {
-        // ACTIVACIÓN DEL SISTEMA AUTOMÁTICO
-        SistemaAlertas sistema = new SistemaAlertas();
-        sistema.iniciarMonitoreoAutomatico(); 
-
         MenuPrincipal menu = new MenuPrincipal();
         menu.mostrarMenu();
     }
